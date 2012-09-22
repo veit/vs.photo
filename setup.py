3941ae2ff153618939c475ed7bebee9688dcda6f
Veit Schiele <kontakt@veit-schiele.de>
2012-09-22 16:24:07 +0200

    Added vs.photo

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.0'
+
+setup(name='vs.photo',
+      version=version,
+      description="Extended support for images in Plone",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Veit Schiele',
+      author_email='kontakt@veit-schiele.de',
+      url='http://pypi.python.org/pypi/vs.photo',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['vs'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
